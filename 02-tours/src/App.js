5ecc96b3a6907738b0b197b959e807fa290657b8
wBlanck <blanck.william@gmail.com>
2022-08-03 12:26:35 +0200

    added button logicv

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -6,15 +6,32 @@ import Tours from "./Tours";
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    console.log(id);
+    const updatedTours = tours.filter((tour) => tour.id !== id);
+    setTours(updatedTours);
+  };
+
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   return (
     <main>
-      <section>
-        <div className="title">
-          <h2>our tours</h2>
-          <div className="underline"></div>
-        </div>
-        <div></div>
-      </section>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
--- 02-tours/src/Tour.js
+++ 02-tours/src/Tour.js
@@ -1,7 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, name, info, image, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+
+        <p>
+          {!readMore ? `${info.slice(0, 200)}...` : `${info}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            Read {readMore ? "Less" : "More"}
+          </button>
+        </p>
+
+        <button
+          className="delete-btn"
+          onClick={() => {
+            removeTour(id);
+          }}>
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
