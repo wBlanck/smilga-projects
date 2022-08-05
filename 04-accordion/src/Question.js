afba747d54beafefdec2fb96456fa60c140a4dd3
wblanck <blanck.william@gmail.com>
2022-08-05 09:38:54 +0200

    finito

--- 04-accordion/src/Question.js
+++ 04-accordion/src/Question.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, info, title }) => {
+  const [toggleInfo, setToggleInfo] = useState(false);
+
+  return (
+    <section className="info">
+      <article className="question">
+        <header>
+          <h4>{title}</h4>
+          <button
+            className="btn"
+            onClick={() => setToggleInfo((prev) => !prev)}>
+            {toggleInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          </button>
+        </header>
+        {toggleInfo && <p>{info}</p>}
+      </article>
+    </section>
+  );
 };
 
 export default Question;
