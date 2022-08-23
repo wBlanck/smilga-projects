28dab9fa546c4b87340e704e1c24e6490843eca9
wblanck <blanck.william@gmail.com>
2022-08-23 05:20:34 +0200

    WIP

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [userColor, setUserColor] = useState("#fff");
+
+  const color = new Values(userColor),
+    { log } = console;
+
+  log(color.shades());
+  log(color.tints());
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form>
+          <input type="text" className="null" placeholder="green" value />
+          <button type="submit" className="btn">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <article
+          className="color false"
+          style={{ backgroundColor: "rgb(255,255,2)" }}>
+          <p className="percent-value"></p>
+          <p className="color-value"></p>
+        </article>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
