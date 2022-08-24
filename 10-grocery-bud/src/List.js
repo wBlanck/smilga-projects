f6923fce00206cd0a861d60fb9f29b2de29420d8
wblanck <blanck.william@gmail.com>
2022-08-24 05:08:21 +0200

    markup added

--- 10-grocery-bud/src/List.js
+++ 10-grocery-bud/src/List.js
@@ -1,7 +1,24 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 const List = () => {
-  return <h2>list component</h2>
-}
+  return (
+    <div className="grocery-container">
+      <div className="grocery-list">
+        <article className="grocery-item">
+          <p className="title">eggs</p>
+          <div className="btn-container">
+            <button type="button" className="edit-btn">
+              <FaEdit />
+            </button>
+            <button type="button" className="delete-btn">
+              <FaTrash />
+            </button>
+          </div>
+        </article>
+      </div>
+      <button className="clear-btn">clear items</button>
+    </div>
+  );
+};
 
-export default List
+export default List;
